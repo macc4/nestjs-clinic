d3186855d92635924708919e02d221f19de83eae
Aleksei Leonenko <alekseileonenko98@gmail.com>
2021-10-08 21:09:21 +0300

    week-1: add docs for auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,18 +1,46 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 
+@ApiTags('authorization')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  //
+  // Sign up as patient
+  //
+
   @Post('/signup')
+  @ApiOperation({ summary: 'Sign in as patient' })
+  @ApiResponse({
+    status: 201,
+    description: 'Returns the jwt token',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Returns Conflict if user already exists',
+  })
   signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
     return this.authService.signUp(signUpDto);
   }
 
+  //
+  // Sign in
+  //
+
   @Post('/signin')
+  @ApiOperation({ summary: 'Sign in' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the jwt token',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Returns Not Unauthorized if input data is invalid',
+  })
   signIn(@Body() signInDto: SignInDto): Promise<{ token: string }> {
     return this.authService.signIn(signInDto);
   }
