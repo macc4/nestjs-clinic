29da5a2020e19ef6ba04579bc8ce85d6761dc325
Aleksei <alekseileonenko98@gmail.com>
2022-01-05 16:26:22 +0200

    fix: remove comments

--- apps/lambda/src/app.service.ts
+++ apps/lambda/src/app.service.ts
@@ -9,34 +9,11 @@ export class AppService {
   constructor(private readonly configService: ConfigService) {}
 
   async getParametersFromSsmAndLog(): Promise<string> {
-    // const ssmConfig = {};
-
-    // const result = await ssmClient
-    //   .getParametersByPath({
-    //     Path: '/itrex/clinic/lambda/',
-    //     Recursive: true,
-    //   })
-    //   .promise();
-
-    // result.Parameters.forEach(ssmParameter => {
-    //   ssmConfig[ssmParameter.Name.split('/itrex/clinic/lambda/')[1]] =
-    //     ssmParameter.Value;
-    // });
-
     console.log('\n');
-
-    console.log(this.configService);
     console.log(this.configService.get('TEST_PARAMETER'));
     console.log('\n');
 
-    // console.log('\n');
-    // console.log(ssmConfig);
-    // console.warn(ssmConfig);
-    // console.log(ssmConfig);
-    // console.warn(ssmConfig);
-    // console.log('\n');
-
-    const returnString = `Hello from Lambda! SSM Parameters: \n ${this.configService.get(
+    const returnString = `Hello from Lambda! SSM TEST_PARAMETER: \n ${this.configService.get(
       'TEST_PARAMETER',
     )}`;
 
